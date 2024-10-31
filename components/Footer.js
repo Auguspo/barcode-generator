b8ef91a89093d849364b894d2520e7667072e22f
Auguspo <augustpolet@gmail.com>
2024-10-31 00:34:50 -0300

    footer added

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="bg-gray-200 text-center py-4 fixed bottom-0 left-0 w-full">
+      <p className='text-sm text-gray-700'>
+        Código generado respetando el ANEXO I RESOLUCIÓN GENERAL N° 1.702
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
