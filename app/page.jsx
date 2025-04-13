4fe152f0ebffc1f2fb120897aa4e39033c5feb21
Auguspo <augustpolet@gmail.com>
2025-04-12 21:11:23 -0300

    Update fix lenght ITF

--- app/page.jsx
+++ app/page.jsx
@@ -219,8 +219,8 @@ const BarcodePage = () => {
       return; // No permite cambios si no cumple la longitud
     }
 
-    if (puntoVenta.length !== 4) {
-      alert("El Punto de Venta debe tener exactamente 4 caracteres.");
+    if (puntoVenta.length !== 5) {
+      alert("El Punto de Venta debe tener exactamente 5 caracteres.");
       return; // No permite cambios si no cumple la longitud
     }
 
